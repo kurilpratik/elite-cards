9e1a4d6e1af04b0468a39eea937fd5a773383d75
Pratik Kuril <pratikkuril16@gmail.com>
2025-10-07 15:50:33 +0530

    Add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
-      <h3>Elite Cards</h3>
-    </div>
+    <main className="container">
+      <section className="grid h-[80vh] place-items-center">
+        <h3 className="text-3xl">Elite Cards</h3>
+      </section>
+    </main>
   );
 }
---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import React from "react";
+
+type NavLink = { label: string; href: string };
+
+const navLinks: NavLink[] = [
+  { label: "Home", href: "/" },
+  { label: "Cards", href: "/cards" },
+  { label: "Contact", href: "/contact" },
+];
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between px-8 py-4">
+      <Link href={"/"} className="text-xl">
+        <span className="font-bold">Elite</span>Cards
+      </Link>
+      <ul>
+        {navLinks.map((link) => (
+          <Link href={link.href} key={link.href} className="ml-8">
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
